cf07ecb72139a6d4e495726e50d15955bee2f7b2
Elijah <chilesheelijah01@gmail.com>
2022-12-25 09:19:03 +0200

    Added CSS Style and Blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        {title: 'Software Engineering', body: 'lorem ipsum...', author: 'Peter', id: 1},
+        {title: 'Database and Web Dev', body: 'lorem ipsum...', author: 'Elijah', id: 2},
+        {title: 'Java Programming', body: 'lorem ipsum...', author: 'Mapalo', id: 3}
+    ]);
+
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written By { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
