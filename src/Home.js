402ea54d9d56bf9281a5e4d209178ae84ddfbd56
Elijah <chilesheelijah01@gmail.com>
2022-12-27 18:37:00 +0200

    Created custom Hook to Fetch Data

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,11 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./usseFetch";
 
 
 const Home = () => {
 
-    // Update the setBlogs State with the data ---------> STEP 2
-    const [blogs, setBlogs] = useState( null );
-    const [isPending, setIsPending] = useState( true );
-    const [error, setError] = useState( null )
-
-
-    // Use Effect Function to fecth the data from the blogs API -------> STEP 1
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Failed to Fetch the data')
-                }
-
-                return res.json();
-            })
-            .then(data => {
-                console.log(data)
-                // Update setBlogs with the fetched data  
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            //  Add Error Catch Msg
-            .catch(err => {
-                setIsPending(false)
-                setError(err.message)
-                setBlogs(null)
-            })
-        }, 1000)
-    }, [])
+    const { data, isPending, error } = useFetch('http://localhost:8000/blogs');
 
 // render the 
     return (
@@ -47,7 +17,7 @@ const Home = () => {
             {isPending && <div>Loading...</div>}
 
             {/* Add Blogs Prop */}
-            { blogs && <BlogList blogs={blogs}/>}
+            { data && <BlogList blogs={data}/>}
         </div>
     );
 }
---
+++ src/usseFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+
+
+    // Update the setBlogs State with the data ---------> STEP 2
+    const [data, setData] = useState( null );
+    const [isPending, setIsPending] = useState( true );
+    const [error, setError] = useState( null )
+
+
+    // Use Effect Function to fecth the data from the blogs API -------> STEP 1
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Failed to Fetch the data')
+                }
+
+                return res.json();
+            })
+            .then(data => {
+                console.log(data)
+                // Update setBlogs with the fetched data  
+                setData(data)
+                setIsPending(false)
+                setError(null)
+            })
+            //  Add Error Catch Msg
+            .catch(err => {
+                setIsPending(false)
+                setError(err.message)
+                setData(null)
+            })
+        }, 1000)
+    }, [url]);
+
+    return { data, isPending, error }
+
+}
+
+export default useFetch;
